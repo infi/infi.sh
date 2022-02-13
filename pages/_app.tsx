3a5eb9cf685a367a1e8a04c7445a9ba8521f8fc4
Infi <wingit@geist.ga>
2022-02-13 23:53:27 +0100

    feat: update favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,7 +11,7 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
                 <meta property="og:locale" content="en" />
                 <meta property="og:site_name" content="Infi" />
                 <meta name="theme-color" content="#28202f" />
-                <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+                <link rel="icon" href="/favicon.png" type="image/png" />
             </Head>
             <Component {...pageProps} />
         </>
