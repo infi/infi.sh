f20edafe0e79b1558243e13a5e79abd80c110c8b
Infi <wingit@geist.ga>
2022-04-26 21:55:55 +0200

    feat: add twitter card format

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,6 +10,7 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
                 <title>Infi</title>
                 <meta property="og:locale" content="en" />
                 <meta property="og:site_name" content="Infi" />
+                <meta name="twitter:card" content="summary_large_image" />
                 <meta name="theme-color" content="#28202f" />
                 <link rel="icon" href="/favicon.png" type="image/png" />
             </Head>
